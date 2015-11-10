783a7c00caaf6a4d93dbfd3825ccdee6ef1e8041
Dmitry Romakhin <romakhin@lexion-development.ru>
2015-11-10 15:00:20 +0300

    Диаграмма Ганта - уебанская версия

--- lma/projects/views_history.py
+++ lma/projects/views_history.py
@@ -34,26 +34,33 @@ def gantt(project_id):
     tasks = []
     history = {}
 
+    SCALE = 6000
+
     options = forms.OutputOptions(request.args)
     for task, seen_ in project.get_tasks_query(options):
         tasks.append(task)
-        history[task.id] = [(
-            int((task.created - project.created).total_seconds() / 18000),
-            'open'
-        )]
+        history[task.id] = [[
+            'open',
+            int((task.created - project.created).total_seconds() / SCALE),
+            0
+        ]]
 
     history_q = TaskHistory.query.join(Task)\
         .filter(Task.project_id == project.id)\
         .filter(TaskHistory.status != None)\
-        .order_by(TaskHistory.created)
+        .order_by(TaskHistory.task_id, TaskHistory.created)
+    max_time = 0
     for h in history_q.all():
-        history.setdefault(h.task_id, []).append((
-            int((h.created - project.created).total_seconds() / 18000),
-            h.status
-        ))
+        pos = int((h.created - project.created).total_seconds() / SCALE)
+        history.setdefault(h.task_id, [])
+        if len(history[h.task_id]):
+            history[h.task_id][-1][2] = pos - history[h.task_id][-1][1]
+        history[h.task_id].append([h.status, pos, 0])
+        if pos > max_time:
+            max_time = pos
 
     g.now = datetime.now(tz=pytz.timezone('Europe/Moscow'))
     g.IMPORTANCE = IMPORTANCE
     g.CHARACTERS = CHARACTERS
 
-    return render_template('projects/gantt.html', project=project, tasks=tasks, history=history)
+    return render_template('projects/gantt.html', project=project, tasks=tasks, history=history, max_time=max_time)
