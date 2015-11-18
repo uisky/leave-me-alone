824c73f81d29a746545a3c4b9e313e550d67dd77
Dmitry Romakhin <romakhin@lexion-development.ru>
2015-11-18 12:40:02 +0300

    lunch timer fix

--- lma/static/plugin-lunch.js
+++ lma/static/plugin-lunch.js
@@ -6,9 +6,13 @@
     lunchtime.setSeconds(0);
 
     setInterval(function(){
-        var now = new Date(), d = (lunchtime.valueOf() - now.valueOf()) / 1000,
-            h = d / 3600 % 24, m = d / 60 % 60, s = d % 60;
-        $el.html(sprintf('До обеда <b>%02d:%02d:%02d', h, m, s));
+        var now = new Date(), d = (lunchtime.valueOf() - now.valueOf()) / 1000;
+        if(d < 0) {
+            $el.html('');
+        } else {
+            var h = d / 3600 % 24, m = d / 60 % 60, s = d % 60;
+            $el.html(sprintf('До обеда <b>%02d:%02d:%02d', h, m, s));
+        }
     }, 1000);
 
 })();
