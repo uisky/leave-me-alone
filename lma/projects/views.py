fc8c049d6ef26f8ec671638107914fd93133e85b
Dmitry Romakhin <romakhin@lexion-development.ru>
2016-10-26 17:07:07 +0300

    Убрал упоминания о неиерархических проектах

--- lma/projects/views.py
+++ lma/projects/views.py
@@ -98,49 +98,18 @@ def folder_delete(folder_id):
     return redirect(url_for('.index'))
 
 
-@mod.route('/<project_id>/about/')
-def about(project_id):
-    project, membership = load_project(project_id)
-
-    edit = request.args.get('edit')
-    if edit:
-        editing = ProjectMember.query.get_or_404((edit, project.id))
-    else:
-        editing = ProjectMember()
-
-    members = ProjectMember.query\
-        .filter(ProjectMember.project_id == project.id)\
-        .order_by(ProjectMember.karma.desc(), ProjectMember.added)\
-        .options(db.joinedload(ProjectMember.user))\
-        .all()
-
-    stat = {}
-    query = db.session\
-        .query(
-            db.func.coalesce(Task.assigned_id, Task.user_id).label('worker_id'),
-            Task.status,
-            db.func.count('*'),
-        )\
-        .filter(Task.project_id == project.id, Task.status != None)\
-        .group_by('worker_id', Task.status)
-    for worker_id, status, cnt in query.all():
-        stat.setdefault(worker_id, {})[status] = cnt
-
-    return render_template('projects/about.html', project=project, members=members, editing=editing, stat=stat, ProjectMember=ProjectMember)
-
-
 @mod.route('/add', methods=('POST',), endpoint='project_add')
 def project_add():
-    project = Project(user_id=current_user.id)
+    project = Project(user_id=current_user.id, type='tree')
 
     project.name = request.form.get('name', '').strip()
     if project.name == '':
         flash('Проекту нужно имя!')
         return redirect(url_for('.index'))
 
-    project.type = request.form.get('type', 'tree')
-    if project.type not in Project.TYPES.keys():
-        project.type = 'tree'
+    # project.type = request.form.get('type', 'tree')
+    # if project.type not in Project.TYPES.keys():
+    #     project.type = 'tree'
 
     db.session.add(project)
     db.session.flush()
@@ -181,6 +150,37 @@ def project_edit(project_id=None):
         project=project, form=form,
         sprints=sprints
     )
+
+
+@mod.route('/<project_id>/about/')
+def about(project_id):
+    project, membership = load_project(project_id)
+
+    edit = request.args.get('edit')
+    if edit:
+        editing = ProjectMember.query.get_or_404((edit, project.id))
+    else:
+        editing = ProjectMember()
+
+    members = ProjectMember.query\
+        .filter(ProjectMember.project_id == project.id)\
+        .order_by(ProjectMember.karma.desc(), ProjectMember.added)\
+        .options(db.joinedload(ProjectMember.user))\
+        .all()
+
+    stat = {}
+    query = db.session\
+        .query(
+            db.func.coalesce(Task.assigned_id, Task.user_id).label('worker_id'),
+            Task.status,
+            db.func.count('*'),
+        )\
+        .filter(Task.project_id == project.id, Task.status != None)\
+        .group_by('worker_id', Task.status)
+    for worker_id, status, cnt in query.all():
+        stat.setdefault(worker_id, {})[status] = cnt
+
+    return render_template('projects/about.html', project=project, members=members, editing=editing, stat=stat, ProjectMember=ProjectMember)
 
 
 @mod.route('/<int:project_id>/delete/', methods=('POST',))
@@ -278,17 +278,3 @@ def sprint_delete(project_id, sprint_id=None):
     db.session.commit()
 
     return redirect(url_for('.sprints', project_id=project.id))
-
-
-@mod.route('/<int:project_id>/type', methods=('POST',))
-def set_type(project_id):
-    project, membership = load_project(project_id)
-
-    type_ = request.form.get('type')
-    if type_ not in Project.TYPES.keys():
-        abort(400)
-
-    project.type = type_
-    db.session.commit()
-
-    return redirect(url_for('.tasks', project_id=project.id))
