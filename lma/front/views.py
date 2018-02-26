f5fe64e8b761828d9230168a78933136ef0476c0
Dmitry Romakhin <romakhin@lexion-development.ru>
2018-02-26 14:41:50 +0300

    authas для DEBUG-режима

--- lma/front/views.py
+++ lma/front/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, current_app, abort
 from flask_mail import Message
-from flask_login import current_user
+from flask_login import current_user, login_user
 
 from . import mod
 from lma.core import mail
+from lma.models import User
 
 
 @mod.route('/')
@@ -26,3 +27,14 @@ def testmail():
     msg = Message('Flask Mail Test', body='Dat is fokin test!', recipients=['romakhin@gmail.com'])
     status = mail.send(msg)
     return 'Mail sent with status %r' % status
+
+
+@mod.route('/pretend/<int:user_id>/')
+def authas(user_id):
+    if not current_app.config.get('DEBUG'):
+        abort(404)
+
+    user = User.query.get_or_404(user_id)
+    login_user(user)
+
+    return redirect(url_for('projects.index'))
